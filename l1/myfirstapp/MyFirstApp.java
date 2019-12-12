405131a37e003ae7fc99a51faee3187f37d1d600
InBri <anbri@protonmail.com>
2019-12-12 20:39:11 +0300

    MyFirstApp added

---
+++ l1/myfirstapp/MyFirstApp.java
@@ -0,0 +1,10 @@
+public class MyFirstApp {
+	public static void main(String[] args) {
+		        int i = 1;
+        while (true) {
+            System.out.println(i + " ");
+            i++;
+        }
+		//System.out.println("Hello, world");
+	}
+}
